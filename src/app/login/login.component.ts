fd2ae14dc12ee2bdb08c49f1e309921b78228f16
Ed Morales <eduardo.morales@Teradata.com>
2016-09-14 15:35:05 -0700

    added main loading mask in login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { TdLoadingService } from '@covalent/core';
+
 @Component({
   moduleId: module.id,
   selector: 'qs-login',
@@ -12,10 +14,15 @@ export class LoginComponent {
   username: string;
   password: string;
 
-  constructor(private _router: Router) {}
+  constructor(private _router: Router,
+              private _loadingService: TdLoadingService) {}
 
   login(): void {
+    this._loadingService.register('main');
     alert('Mock log in as ' + this.username);
-    this._router.navigate(['/']);
+    setTimeout(() => {
+      this._router.navigate(['/']);
+      this._loadingService.resolve('main');
+    }, 2000);
   }
 }
