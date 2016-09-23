defd055e4146057b1ae63df9af57e9beec05e280
Ed Morales <eduardo.morales@Teradata.com>
2016-09-23 13:09:03 -0700

    added RESTService, loading mask and delete workaround

--- src/app/users/+form/form.component.ts
+++ src/app/users/+form/form.component.ts
@@ -13,29 +13,30 @@ import { UsersService, IUser } from '../../../services';
 })
 export class UsersFormComponent implements OnInit  {
 
-  constructor(private _usersService: UsersService, private _route: ActivatedRoute) {}
   display_name: string;
   email: string;
-	id: string;
+  id: string;
   admin: boolean;
   user: IUser;
   action: string;
 
-  goBack(){
+  constructor(private _usersService: UsersService, private _route: ActivatedRoute) {}
+
+  goBack(): void {
     window.history.back();
   }
 
   ngOnInit(): void {
     this._route.url.subscribe(url => {
-      this.action = (url.length > 1 ? url[1].path : "add");
+      this.action = (url.length > 1 ? url[1].path : 'add');
     });
     this._route.params.subscribe((params: {id: string}) => {
       let userId: string = params.id;
       this._usersService.get(userId).subscribe((user: any) => {
         this.display_name = user.display_name;
         this.email = user.email;
-        this.admin = (user.site_admin == 1 ? true : false);
-				this.id = user.id;
+        this.admin = (user.site_admin === 1 ? true : false);
+        this.id = user.id;
       });
     });
   }
@@ -49,9 +50,16 @@ export class UsersFormComponent implements OnInit  {
       site_admin: site_admin,
       id: this.id || this.display_name.replace(/\s+/g, '.'),
       created: now,
-      last_access: now,  
+      last_access: now,
+    };
+    if (this.action === 'add') {
+      this._usersService.create(this.user).subscribe(() => {
+        this.goBack();
+      });
+    } else {
+      this._usersService.update(this.id, this.user).subscribe(() => {
+        this.goBack();
+      });
     }
-    this._usersService.update(this.user, this.action).subscribe();
-    this.goBack();
   }
 }
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Response, Headers, RequestOptions } from '@angular/http';
-import { HttpInterceptorService } from '@covalent/http';
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Subscriber } from 'rxjs/Subscriber';
+
+import { HttpInterceptorService, RESTService } from '@covalent/http';
 
 export interface IUser {
   display_name: string;
@@ -12,58 +15,40 @@ export interface IUser {
 }
 
 @Injectable()
-export class UsersService {
+export class UsersService extends RESTService<IUser> {
 
-  private staticApiData: string = 'data/users.json';
-  private mockApiData: string = 'http://localhost:8080/users';
-
-  constructor(private _http: HttpInterceptorService) {}
-
-  staticQuery(): any {
-    return this._http.get(this.staticApiData)
-    .map((res: Response) => {
-      return res.json();
+  constructor(private _http: HttpInterceptorService) {
+    super(_http, {
+      baseUrl: 'http://localhost:8080',
+      path: '/users',
     });
   }
 
-  query(): any {
-    return this._http.get(this.mockApiData)
-    .map((res: Response) => {
-      return res.json();
-    });
-  }
-
-  get(id: string): any {
-    return this._http.get(this.mockApiData)
-    .map((res: Response) => {
-      let user: any;
-      res.json().forEach((s: any) => {
-        if (s.id === id) {
-          user = s;
+  public delete(id: string | number): Observable<any> {
+    let request: Observable<Response> = this.http.delete(this.buildUrl(id), this.buildRequestOptions());
+    return request.map((res: Response) => {
+      if (res.status === 200) {
+        try {
+          return this.transform(res);
+        } catch (e) {
+          return res;
+        }
+      } else {
+        return res;
+      }
+    }).catch<any>((error: Response) => {
+      return new Observable<any>((subscriber: Subscriber<any>) => {
+        try {
+          subscriber.error(this.transform(error));
+        } catch (err) {
+          subscriber.error(error);
         }
       });
-      return user;
     });
   }
 
-  update(user: IUser, action: string): any {
-    let body = JSON.stringify(user);
-    if (action == "add") {
-      return this._http.post(this.mockApiData, body)
-      .map((res: Response) => {
-        return res.json();
-      });
-    };
-    if (action == "edit") {
-      return this._http.put(this.mockApiData + "/" + user.id, body)
-      .map((res: Response) => {
-        return res.json();
-      });
-    };
-  }
-
-  deleteUser(id: string): any {
-    return this._http.delete(this.mockApiData + "/" + id)
+  staticQuery(): Observable<IUser[]> {
+    return this._http.get('data/users.json')
     .map((res: Response) => {
       return res.json();
     });
