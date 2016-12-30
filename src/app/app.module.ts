1209a075535d7f00c7112bb1cb04d669df2a61fd
Ed Morales <eduardo.morales@Teradata.com>
2016-12-30 14:56:00 -0800

    imported single core package + http config breaking changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Type } from '@angular/core';
 import { BrowserModule, Title }  from '@angular/platform-browser';
 
-import { CovalentCoreModule, TD_LOADING_ENTRY_COMPONENTS } from '@covalent/core';
-import { CovalentChipsModule } from '@covalent/chips';
-import { CovalentFileModule } from '@covalent/file-upload';
-import { CovalentHttpModule } from '@covalent/http';
+import { CovalentCoreModule } from '@covalent/core';
+import { CovalentHttpModule, IHttpInterceptor } from '@covalent/http';
 import { CovalentHighlightModule } from '@covalent/highlight';
-import { CovalentJsonFormatterModule } from '@covalent/json-formatter';
 import { CovalentMarkdownModule } from '@covalent/markdown';
 import { CovalentChartsModule } from '@covalent/charts';
-import { CovalentDataTableModule } from '@covalent/data-table';
-import { CovalentPagingModule } from '@covalent/paging';
-import { CovalentSearchModule } from '@covalent/search';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
@@ -33,6 +27,10 @@ import { ChartComponent } from '../components/chart/chart.component';
 import { ButtonDisableFix } from '../directives/button-disable-fix.directive';
 
 import { RequestInterceptor } from '../config/interceptors/request.interceptor';
+
+const httpInterceptorProviders: Type<IHttpInterceptor>[] = [
+  RequestInterceptor,
+];
 
 @NgModule({
   declarations: [
@@ -58,22 +56,21 @@ import { RequestInterceptor } from '../config/interceptors/request.interceptor';
     BrowserModule,
     CovalentCoreModule.forRoot(),
     CovalentChartsModule.forRoot(),
-    CovalentChipsModule.forRoot(),
-    CovalentDataTableModule.forRoot(),
-    CovalentFileModule.forRoot(),
-    CovalentHttpModule.forRoot([RequestInterceptor]),
+    CovalentHttpModule.forRoot({
+      inteceptors: [{
+        interceptor: RequestInterceptor, paths: ['**'],
+      }],
+    }),
     CovalentHighlightModule.forRoot(),
-    CovalentJsonFormatterModule.forRoot(),
     CovalentMarkdownModule.forRoot(),
-    CovalentPagingModule.forRoot(),
-    CovalentSearchModule.forRoot(),
     appRoutes,
   ], // modules needed to run this module
   providers: [
     appRoutingProviders,
+    httpInterceptorProviders,
     Title,
   ], // additional providers needed for this module
-  entryComponents: [ TD_LOADING_ENTRY_COMPONENTS ],
+  entryComponents: [ ],
   bootstrap: [ AppComponent ],
 })
 export class AppModule {}
