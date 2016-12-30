7d28478df17a263b9493d47892949c3a48bc7430
Ed Morales <eduardo.morales@Teradata.com>
2016-12-30 14:56:26 -0800

    use relative path for svg loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,21 +21,21 @@ export class AppComponent {
     };
     this._loadingService.createOverlayComponent(options, viewContainerRef);
     this._iconRegistry.addSvgIconInNamespace('assets', 'teradata',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/teradata.svg'));
+      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/teradata.svg'));
     this._iconRegistry.addSvgIconInNamespace('assets', 'github',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/github.svg'));
+      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/github.svg'));
     this._iconRegistry.addSvgIconInNamespace('assets', 'covalent',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/covalent.svg'));
+      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/covalent.svg'));
     this._iconRegistry.addSvgIconInNamespace('assets', 'covalent-mark',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/covalent-mark.svg'));
+      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/covalent-mark.svg'));
     this._iconRegistry.addSvgIconInNamespace('assets', 'teradata-ux',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/teradata-ux.svg'));
+      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/teradata-ux.svg'));
     this._iconRegistry.addSvgIconInNamespace('assets', 'appcenter',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/appcenter.svg'));
+      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/appcenter.svg'));
     this._iconRegistry.addSvgIconInNamespace('assets', 'listener',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/listener.svg'));
+      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/listener.svg'));
     this._iconRegistry.addSvgIconInNamespace('assets', 'querygrid',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('/assets/icons/querygrid.svg'));
+      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/querygrid.svg'));
   }
 
 }
