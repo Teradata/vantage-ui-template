8b54cf21655838140014e2c3f947440fcb6b0d58
emoralesb05 <eduardo.morales@teradata.com>
2017-02-26 11:23:11 -0800

    fix(): remove .angular-cli.json ref from karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,6 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
    },
     angularCli: {
-      config: './.angular-cli.json',
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
