afce26592d5453030dc5f94f92f8b9e045a5e4f8
Alexander Merkulov <sasha@merqlove.ru>
2017-03-01 07:25:00 +0300

    Update jasmine-spec-reporter

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,7 +2,7 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 /*global jasmine */
-let SpecReporter = require('jasmine-spec-reporter');
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 
 exports.config = {
   allScriptsTimeout: 11000,
