a90030436c1b14b411228f6185fa6dba38dfafc2
Jose Toledo <joset2.91@gmail.com>
2017-05-28 16:20:16 +0200

    Core(): updated dependencies
    * Updated to latest flexlayout/material packages
    * Updated @covalent packages
    * Updated angular cli version
    * Updated d3/ngx charts
    
    Fixed(themes): fixed import
    
    * Fixed import of material theming
    
    Refactored(routing): added routing module
    
    * Added a routing module for the application to capsulate the configuration of the RouterModule
    
    Feat(): added shared module
    
    * Added a shared module that contains all the elements that are shared across the different application modules
    
    Refactored(app module): changed how the module is configurated
    
    * Moved import of routed components into app-routing.module
    * Moved styling modules into shared module
    * Others
    
    Refactored(templates component): changes in html template
    
    * Added button directive to nav links
    * Refactored div elements into routed links in the covalent templates section
    
    Core(): added missing script command
    
    * Added ng serve on start command
    
    Fixed(editor component): added missing directive in html template
    
    * Added flex layout align directive
    
    Fixed(shared module): added missing imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,16 @@ import { NgModule, Type } from '@angular/core';
 import { BrowserModule, Title }  from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { CovalentCoreModule } from '@covalent/core';
 import { CovalentHttpModule, IHttpInterceptor } from '@covalent/http';
 import { CovalentHighlightModule } from '@covalent/highlight';
 import { CovalentMarkdownModule } from '@covalent/markdown';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UsersComponent } from './users/users.component';
-import { UsersFormComponent } from './users/form/form.component';
-import { LogsComponent } from './logs/logs.component';
-import { FormComponent } from './form/form.component';
-import { DetailComponent } from './detail/detail.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardProductComponent } from './dashboard-product/dashboard-product.component';
-import { ProductOverviewComponent } from './dashboard-product/overview/overview.component';
-import { ProductStatsComponent } from './dashboard-product/stats/stats.component';
-import { ProductFeaturesComponent } from './dashboard-product/features/features.component';
-import { FeaturesFormComponent } from './dashboard-product/features/form/form.component';
-import { TemplatesComponent } from './templates/templates.component';
-import { DashboardTemplateComponent } from './templates/dashboard/dashboard.component';
-import { EmailTemplateComponent } from './templates/email/email.component';
-import { EditorTemplateComponent } from './templates/editor/editor.component';
-import { appRoutes, appRoutingProviders } from './app.routes';
-
 import { RequestInterceptor } from '../config/interceptors/request.interceptor';
 
-import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { routedComponents, AppRoutingModule } from './app-routing.module';
+
+import { SharedModule } from './shared/shared.module';
 
 const httpInterceptorProviders: Type<any>[] = [
   RequestInterceptor,
@@ -38,28 +20,13 @@ const httpInterceptorProviders: Type<any>[] = [
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    DashboardComponent,
-    DashboardProductComponent,
-    ProductOverviewComponent,
-    ProductStatsComponent,
-    ProductFeaturesComponent,
-    FeaturesFormComponent,
-    UsersComponent,
-    UsersFormComponent,
-    LogsComponent,
-    FormComponent,
-    DetailComponent,
-    LoginComponent,
-    TemplatesComponent,
-    DashboardTemplateComponent,
-    EmailTemplateComponent,
-    EditorTemplateComponent,
+    routedComponents,
   ], // directives, components, and pipes owned by this NgModule
   imports: [
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
-    CovalentCoreModule,
+    SharedModule,
     CovalentHttpModule.forRoot({
       interceptors: [{
         interceptor: RequestInterceptor, paths: ['**'],
@@ -67,11 +34,8 @@ const httpInterceptorProviders: Type<any>[] = [
     }),
     CovalentHighlightModule,
     CovalentMarkdownModule,
-    appRoutes,
-    NgxChartsModule,
   ], // modules needed to run this module
   providers: [
-    appRoutingProviders,
     httpInterceptorProviders,
     Title,
   ], // additional providers needed for this module
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,67 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule, } from '@angular/flex-layout';
+import {
+  CovalentDataTableModule, CovalentMediaModule, CovalentLoadingModule,
+  CovalentNotificationsModule, CovalentLayoutModule, CovalentMenuModule,
+  CovalentPagingModule, CovalentSearchModule, CovalentStepsModule,
+  CovalentCommonModule, CovalentDialogsModule,
+} from '@covalent/core';
+import {
+  MdButtonModule, MdCardModule, MdIconModule,
+  MdListModule, MdMenuModule, MdTooltipModule,
+  MdSlideToggleModule, MdInputModule, MdCheckboxModule,
+  MdToolbarModule, MdSnackBarModule, MdSidenavModule,
+  MdTabsModule, MdSelectModule,
+} from '@angular/material';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+const FLEX_LAYOUT_MODULES = [
+  FlexLayoutModule,
+];
+
+const ANGULAR_MODULES = [
+  FormsModule, ReactiveFormsModule,
+];
+
+const MATERIAL_MODULES = [
+  MdButtonModule, MdCardModule, MdIconModule,
+  MdListModule, MdMenuModule, MdTooltipModule,
+  MdSlideToggleModule, MdInputModule, MdCheckboxModule,
+  MdToolbarModule, MdSnackBarModule, MdSidenavModule,
+  MdTabsModule, MdSelectModule,
+];
+
+const COVALENT_MODULES = [
+  CovalentDataTableModule, CovalentMediaModule, CovalentLoadingModule,
+  CovalentNotificationsModule, CovalentLayoutModule, CovalentMenuModule,
+  CovalentPagingModule, CovalentSearchModule, CovalentStepsModule,
+  CovalentCommonModule, CovalentDialogsModule,
+];
+
+const CHART_MODULES = [
+  NgxChartsModule,
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ANGULAR_MODULES,
+    MATERIAL_MODULES,
+    COVALENT_MODULES,
+    CHART_MODULES,
+    FLEX_LAYOUT_MODULES,
+  ],
+  declarations: [
+
+  ],
+  exports: [
+    ANGULAR_MODULES,
+    MATERIAL_MODULES,
+    COVALENT_MODULES,
+    CHART_MODULES,
+    FLEX_LAYOUT_MODULES,
+  ]
+})
+export class SharedModule { }
