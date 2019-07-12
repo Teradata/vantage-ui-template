eefffd055ea8f6722ef2f1d8680c0278e9630f5e
emoralesb05 <eduardo.morales@teradata.com>
2019-07-11 17:11:55 -0700

    feat(): polish unit test for dashboard

--- src/my-project/app/dashboard/dashboard.component.spec.ts
+++ src/my-project/app/dashboard/dashboard.component.spec.ts
@@ -1,41 +1,55 @@
-import { CovalentHttpModule } from '@covalent/http';
-import { CovalentLoadingModule } from '@covalent/core/loading';
-import { DashboardComponent } from './dashboard.component';
+
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-import { VantageSystemModule } from '@td-vantage/ui-platform/system';
-import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectorRef } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
-import { CovalentMediaModule } from '@covalent/core/media';
-import { VantageErrorService } from '@td-vantage/ui-platform/utilities';
-import { CovalentDialogsModule } from '@covalent/core/dialogs';
-import { MatDialogModule } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-describe(' test case for Dashboard Component ', () => {
+import { CovalentMediaModule } from '@covalent/core/media';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-    let fixture: ComponentFixture<DashboardComponent>;
-    let comp: DashboardComponent;
+import { CovalentHttpModule } from '@covalent/http';
+import { CovalentLoadingModule } from '@covalent/core/loading';
+import { CovalentDialogsModule } from '@covalent/core/dialogs';
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            imports: [ TranslateModule.forRoot(), CovalentMediaModule, CovalentHttpModule.forRoot(), CovalentLoadingModule, VantageSystemModule, 
-                MatDialogModule, CovalentDialogsModule, NoopAnimationsModule],
-            declarations: [ DashboardComponent ],
-            providers: [ ChangeDetectorRef,
-                VantageErrorService,
-            ],
-            schemas: [
-                CUSTOM_ELEMENTS_SCHEMA,
-            ],
-        }).compileComponents().then(() => {
-            fixture = TestBed.createComponent(DashboardComponent);
-            comp = fixture.componentInstance;
-        });
-    }));
+import { VantageSystemModule } from '@td-vantage/ui-platform/system';
+import { VantageUserFeedbackModule } from '@td-vantage/ui-platform/utilities';
 
-    it('Register component', async(() => {
-        fixture.detectChanges();
-        expect(comp).toBeTruthy();
-    }));
+import { TranslateModule } from '@ngx-translate/core';
+
+import { DashboardComponent } from './dashboard.component';
+
+describe('Dashboard Component', () => {
+
+  let fixture: ComponentFixture<DashboardComponent>;
+  let comp: DashboardComponent;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        NoopAnimationsModule,
+        MatDialogModule,
+        MatIconModule,
+        MatToolbarModule,
+        TranslateModule.forRoot(),
+        CovalentMediaModule,
+        CovalentHttpModule.forRoot(),
+        CovalentLoadingModule,
+        CovalentDialogsModule,
+        VantageUserFeedbackModule,
+        VantageSystemModule, 
+      ],
+      declarations: [ DashboardComponent ],
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(DashboardComponent);
+      comp = fixture.componentInstance;
+    });
+  }));
+
+  it('checks if component was instantiated', async(() => {
+    fixture.detectChanges();
+    expect(comp).toBeTruthy();
+  }));
 
 });
