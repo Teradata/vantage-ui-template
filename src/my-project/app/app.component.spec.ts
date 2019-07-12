7e19025986b59c2b3c51347e2d80e19ecc7f601e
Priyanka <aqupriyanka@gmail.com>
2019-07-11 17:12:52 -0700

    feat(test): add example unit tests
    * added 2 test cases
    
    * feat(): polish unit test for dashboard

---
+++ src/my-project/app/app.component.spec.ts
@@ -0,0 +1,22 @@
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+      ],
+      declarations: [
+        AppComponent,
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+    const app: AppComponent = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
---
+++ src/my-project/app/dashboard/dashboard.component.spec.ts
@@ -0,0 +1,55 @@
+
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CovalentMediaModule } from '@covalent/core/media';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { CovalentHttpModule } from '@covalent/http';
+import { CovalentLoadingModule } from '@covalent/core/loading';
+import { CovalentDialogsModule } from '@covalent/core/dialogs';
+
+import { VantageSystemModule } from '@td-vantage/ui-platform/system';
+import { VantageUserFeedbackModule } from '@td-vantage/ui-platform/utilities';
+
+import { TranslateModule } from '@ngx-translate/core';
+
+import { DashboardComponent } from './dashboard.component';
+
+describe('Dashboard Component', () => {
+
+  let fixture: ComponentFixture<DashboardComponent>;
+  let comp: DashboardComponent;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        NoopAnimationsModule,
+        MatDialogModule,
+        MatIconModule,
+        MatToolbarModule,
+        TranslateModule.forRoot(),
+        CovalentMediaModule,
+        CovalentHttpModule.forRoot(),
+        CovalentLoadingModule,
+        CovalentDialogsModule,
+        VantageUserFeedbackModule,
+        VantageSystemModule, 
+      ],
+      declarations: [ DashboardComponent ],
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(DashboardComponent);
+      comp = fixture.componentInstance;
+    });
+  }));
+
+  it('checks if component was instantiated', async(() => {
+    fixture.detectChanges();
+    expect(comp).toBeTruthy();
+  }));
+
+});
