35adc9ec3bb910999f2c929b22c91f35d2ca16b2
Christian Memije <christianmemije@gmail.com>
2020-01-21 14:18:32 -0800

    feat(pre-loader): make style dynamic

--- pre-loader/index.js
+++ pre-loader/index.js
@@ -2,8 +2,14 @@
 
 const fs = require('fs');
 const path = require('path');
+const sass = require('sass');
 
-module.exports = (targetOptions, indexHtml) => {
-  const preLoaderMarkup = fs.readFileSync(path.join(__dirname, 'pre-loader.html'), { encoding: 'utf8' }).toString();
-  return indexHtml.replace('<div id="td-pre-loader"></div>', preLoaderMarkup);
+module.exports = (targetOptions, originalIndexHtml) => {
+  const preLoaderHtml = fs.readFileSync(path.join(__dirname, 'pre-loader.html'), { encoding: 'utf8' }).toString();
+  const preLoaderCss = sass
+    .renderSync({ file: path.join(__dirname, 'pre-loader.scss'), includePaths: ['node_modules'] })
+    .css.toString();
+  const preLoaderHtmlWithCss = preLoaderHtml.replace('<style></style>', `<style>${preLoaderCss}</style>`);
+  const newIndexHtml = originalIndexHtml.replace('<div id="td-pre-loader"></div>', preLoaderHtmlWithCss);
+  return newIndexHtml;
 };
